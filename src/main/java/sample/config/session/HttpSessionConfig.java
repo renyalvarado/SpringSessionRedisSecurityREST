e64ae292025d2a0516ab7a2b8aee12214dcad199
cleteci <renyalvarado@gmail.com>
2016-04-28 16:58:17 -0430

    Init repository based on http://docs.spring.io/spring-session/docs/1.1.1.RELEASE/reference/html5/

---
+++ src/main/java/sample/config/session/HttpSessionConfig.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2002-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sample.config.session;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.session.web.http.HttpSessionStrategy;
+
+// tag::class[]
+@Configuration
+@EnableRedisHttpSession // <1>
+public class HttpSessionConfig {
+
+	@Bean
+	public JedisConnectionFactory connectionFactory() {
+		return new JedisConnectionFactory();  // <2>
+	}
+
+	@Bean
+	public HttpSessionStrategy httpSessionStrategy() {
+		return new HeaderHttpSessionStrategy(); // <3>
+	}
+}
+// end::class[]
